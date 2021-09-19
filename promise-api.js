e7904e55769991d6a5e89646d4b290d2fe2be81e
Robin Correa <robin.correa21@gmail.com>
2021-09-19 16:57:26 +0800

    Correction for promise reject.

--- promise-api.js
+++ promise-api.js
@@ -5,5 +5,5 @@ const p = Promise.resolve({ id: 1 });
 p.then((result) => console.log(result));
 
 // Reject
-// const p = Promise.resolve(new Error('reason for rejection...'));
+// const p = Promise.reject(new Error('reason for rejection...'));
 // p.catch((error) => console.log(error));
